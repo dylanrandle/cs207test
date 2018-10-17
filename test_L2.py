daffdc0e3502aac953cdde02f616569cd228c15a
Dylan Randle <dylanrandle@rogers.com>
2018-10-16 21:52:51 -0400

    initial commit

---
+++ test_L2.py
@@ -0,0 +1,17 @@
+import pytest
+import numpy as np
+from toy_L2 import L2
+
+
+def test_L2_result():
+    assert L2([1,1], [1,1]) == np.sqrt(2)
+    
+def test_L2_weight():
+    assert L2([1,1], [2,2]) == 2*np.sqrt(2)
+    
+def test_L2_dims():
+    with pytest.raises(ValueError):
+        L2([1,1,1], [2,2])
+
+def test_L2_zerolen():
+    assert L2([])==0.0
